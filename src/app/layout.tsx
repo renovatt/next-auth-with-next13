5a02906bfe1e35a37b4162ecc75aaf16659cc741
Will <wlymes@gmail.com>
2023-05-23 13:05:08 -0300

    feat: next auth provider added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+import Providers from '@/components/Providers'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import AppBar from '@/components/AppBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +16,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body className={inter.className}>
+        <Providers>
+          <AppBar />
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
