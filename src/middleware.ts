8498ae189839211caae4da667c968f8ffaa10a04
Will <wlymes@gmail.com>
2023-05-26 09:59:11 -0300

    test: route test deploy

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,6 @@ export { default } from "next-auth/middleware"
 export const config = {
     matcher: [
         "/userPost/:path*",
-        "/anotherPrivateRouter/:path*"
+        // "/anotherPrivateRouter/:path*"
     ]
 }
